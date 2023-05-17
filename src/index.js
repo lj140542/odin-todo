8108273dec73ed9506ff402b6b93a4bffbef75df
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-05-17 14:51:04 +0200

    Makes minor changes to the style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css'
 
 console.log('Odin\'s ToDo');
+
+const addProjectBtn = document.getElementById('add-project');
+const projectList = document.getElementById('projects');
+
+addProjectBtn.addEventListener('click',() => {
+  let project = document.createElement('li');
+  let a = document.createElement('a');
+
+  a.textContent = "Created";
+  project.children;
+  projectList.appendChild(project);
+});
