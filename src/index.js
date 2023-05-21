829520154711c71e2eb3c8c62f40af5c26af1ae0
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-05-21 23:30:09 +0200

    Implements the project manipulations
    - deletion of a project by clicking on the trash button
    - creation of a project by clicking on the '+' button
    - selection of a project by clicking on it's name
    - renaming of a project by clicking on it's name when selected
    - default project cannot be either renamed nor deleted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import './style.css';
 
-const ToDo = (title, done) => {
+const ToDo = (id, title, done) => {
+  const getId = () => id;
   const getTitle = () => title;
   const getDone = () => done;
   const setDone = (isDone) => done = isDone;
-  return { getTitle, getDone, setDone };
+  return { getId, getTitle, getDone, setDone };
 }
 
-const Project = (name, todos) => {
+const Project = (id, name, todos) => {
+  const getId = () => id;
   const getName = () => name;
   const getTodos = () => todos;
+  const setName = (newName) => name = newName;
   const addTodo = (todo) => todos.add(todo);
-  return { getName, getTodos, addTodo };
+  const toString = () => `id : ${id} | name : ${name}`;
+  return { getId, getName, getTodos, setName, addTodo, toString };
 }
 
 const Display = (() => {
@@ -22,6 +26,7 @@ const Display = (() => {
 
     iconElement.setAttribute('name', 'add-outline');
     buttonElement.id = 'add-project';
+    buttonElement.addEventListener('click', () => displayNewProject());
     buttonElement.appendChild(iconElement);
     liElement.classList.add('project');
     liElement.appendChild(buttonElement);
@@ -75,12 +80,16 @@ const Display = (() => {
       tmpElement.textContent = 'Default';
     } else tmpElement.textContent = project.getName();
 
+    tmpElement.addEventListener('click', e => Controller.selectProject(project.getId()));
+    tmpElement.addEventListener('blur', e => Controller.renameProject(e, project.getId()));
     liElement.appendChild(tmpElement);
     tmpElement = document.createElement('ion-icon');
     tmpElement.classList.add('delete-project');
     tmpElement.setAttribute('name', 'trash');
+    tmpElement.addEventListener('click', e => Controller.removeProject(e));
     liElement.appendChild(tmpElement);
     liElement.classList.add('project');
+    liElement.setAttribute('project-id', project.getId());
 
     projectUL.appendChild(liElement);
   };
@@ -108,7 +117,12 @@ const Display = (() => {
     contentDIV.appendChild(todoElement);
   };
   const displayNewProject = () => {
+    let newProject = Controller.createProject();
 
+    document.getElementById('projects').removeChild(document.getElementById('add-project').parentElement);
+    displayProject(newProject);
+    Controller.selectProject(newProject.getId());
+    displayAddProjectButton();
   };
   const displayNewTodo = () => {
 
@@ -118,19 +132,40 @@ const Display = (() => {
 
 const Controller = (() => {
   const createProject = () => {
-
+    let newProject = Project(Controller.getNewProjectId(), 'New Project', []);
+    projects.push(newProject);
+    return newProject;
   };
-  const removeProject = () => {
+  const removeProject = (event) => {
+    let liElement = event.target.parentElement;
+    let projectId = liElement.attributes['project-id'].value;
+    let projectIndex = getProjectIndex(projectId);
 
+    if (projectIndex > -1 && projectIndex < projects.length) {
+      projects.splice(projectIndex, 1);
+      if (liElement.classList.contains('selected')) selectProject(0);
+      liElement.parentElement.removeChild(liElement);
+    }
   };
-  const selectProject = (event) => {
+  const selectProject = (id) => {
+    let liProject = document.querySelector(`.project[project-id="${id}"]`);
 
-  };
-  const selectDefaultProject = () => {
-    let defaultProject = document.getElementById('default-project');
-    if (!defaultProject.classList.contains('selected')) {
-      defaultProject.classList.add('selected');
-      Display.displayTodoList(projects[0].getTodos());
+    if (liProject.classList.contains('selected')) return;
+
+    // unselect the current selected project
+    let selectedProjects = document.getElementsByClassName('selected');
+    for (let i = 0; i < selectedProjects.length; i++) {
+      selectedProjects[i].firstElementChild.contentEditable = 'false';
+      selectedProjects[i].classList.remove('selected');
+    }
+
+    // select the correct project
+    let projectIndex = getProjectIndex(id);
+    if (projectIndex > -1 && projectIndex < projects.length) {
+      // blocks the edition of the default project name
+      if (id > 0) liProject.firstElementChild.contentEditable = 'true';
+      liProject.classList.add('selected');
+      Display.displayTodoList(projects[projectIndex].getTodos());
     }
   };
   const createTodo = () => {
@@ -139,14 +174,34 @@ const Controller = (() => {
   const removeTodo = () => {
 
   };
-  return { createProject, removeProject, selectProject, selectDefaultProject, createTodo, removeTodo };
+  const getNewProjectId = () => {
+    let maxId = 0;
+    projects.forEach(project => {
+      if (project.getId() > maxId) maxId = project.getId();
+    });
+    return maxId + 1;
+  };
+  const getProjectIndex = (id) => {
+    return projects.findIndex((project) => project.getId() == id);
+  }
+  const renameProject = (e, id) => {
+    e.preventDefault();
+    let index = getProjectIndex(id);
+    if (index > -1 && index < projects.length) {
+      if (e.target.innerHTML.replaceAll('&nbsp;', '') == '')
+        e.target.textContent = projects[index].getName();
+      else
+        projects[index].setName(e.target.textContent);
+    }
+    console.log(projects.toString());
+  }
+  return { createProject, removeProject, selectProject, createTodo, removeTodo, getNewProjectId, renameProject };
 })();
 
 const projectUL = document.getElementById('projects');
 const contentDIV = document.getElementById('content');
 
-let defaultProject = Project('#Default', [ToDo('This is a default item', false)]);
-let projects = [defaultProject, Project('New Project', [ToDo('This is a new item', false)])];
+let projects = [Project(0, '#Default', [ToDo(0, 'This is a default item', false)])];
 
 Display.displayProjectList(projects);
-Controller.selectDefaultProject();
+Controller.selectProject(0);
